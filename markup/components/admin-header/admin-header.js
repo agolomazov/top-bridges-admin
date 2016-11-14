065246b86e017ccf971b7d2dd3d5db748a813bbc
Антон Голомазов <golomazov-tosha2012@yandex.ru>
2016-11-14 13:49:33 +0300

    Добавлен блок с главным меню

--- markup/components/admin-header/admin-header.js
+++ markup/components/admin-header/admin-header.js
@@ -1,1 +1,15 @@
+/*eslint-disable*/
+$(document).ready(function(){
+    $(window).on('resize', function (e) {
+        var ww = $(this).width();
 
+        if(ww > 780){
+            $('.nav-menu').removeAttr('style');
+        }
+    });
+
+    $(document).on('click', '.nav-toggle', function(){
+        $('.nav-menu').fadeToggle(300);
+    });
+});
+/*eslint-enable*/
