729c88099e5497c4bac34893fedd6ace35b5ba6d
Антон Голомазов <golomazov-tosha2012@yandex.ru>
2016-11-14 22:18:09 +0300

    Добавлен адаптив для блока top-girls-slider

--- markup/components/top-girls-slider/top-girls-slider.js
+++ markup/components/top-girls-slider/top-girls-slider.js
@@ -1,1 +1,34 @@
+/*eslint-disable*/
+;(function($){
+    var options = {
+        itemsCustom : [
+            [700, 1],
+            [720, 2],
+            [800, 2],
+            [1000, 3],
+            [1200, 3],
+            [1250, 4],
+            [1400, 4]
+        ],
+        navigation: false,
+        responsive: true,
+        responsiveRefreshRate: 0,
+        autoHeight: true,
+        scrollPerPage: true,
+        pagination: false,
+        transitionStyle:"fade",
+        autoPlay : 5000
+    };
+    $(".top-girls-slider-content").owlCarousel(options);
+    var owl = $(".top-girls-slider-content").data('owlCarousel');
 
+    $('.slider-control.prev-btn').on('click', function(){
+        owl.prev();
+    });
+
+    $('.slider-control.next-btn').on('click', function(){
+        owl.next();
+    });
+
+})(jQuery);
+/*eslint-enable*/
